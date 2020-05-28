03ee931dc9e4293520b94f27a82db6b17271f61f
Nicholas Copi <copinf@vcu.edu>
2020-05-28 15:16:03 -0400

    Work on getting vm creation from a golden image working and begin working on web end for api.

---
+++ index.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.get('/',(req,res)=>{
+	res.send('ok');
+});
+
+app.listen(port,()=>{
+	console.log(`Listening on port ${port}.`);
+});
--- ssh.js
+++ ssh.js
@@ -1,12 +1,29 @@
 require('dotenv').config();
+const fs = require('fs');
+const template = fs.readFileSync('template.vmx').toString();
 const NodeSSH = require('node-ssh');
 
 const init = async ()=>{
 	const ssh = await newConnection();
 	const ls = await runCommand(ssh,'ls');
+	await newVM('test2');
+	console.log('disposing...');
 	ssh.dispose();
 }
 
+
+const newVM = async(name)=>{
+	const ssh = await newConnection();
+	const path = process.env.STORE_PATH;
+	const tempFilePath = process.env.temp + '/' + name + '.vmx';
+	const vmxPath = path + '/' + name + '/' + name + '.vmx';
+	fs.writeFileSync(tempFilePath, replace(template,'{{name}}',name));
+	await runCommand(ssh,'mkdir', ['-p',name], path);
+	await runCommand(ssh,'cp', [process.env.IMAGE_PATH,name + '.vmdk'], path + '/' + name);
+	await ssh.putFile(tempFilePath, vmxPath);
+	await runCommand(ssh,'vim-cmd', ['solo/registervm',vmxPath]);
+	fs.unlinkSync(tempFilePath);
+}
 
 const newConnection = async()=>{
 	const ssh = new NodeSSH();
@@ -24,9 +41,18 @@ const newConnection = async()=>{
 	return ssh;
 }
 
-const runCommand = async(ssh,command,args)=>{
+const replace = (template,tag,filler) =>{
+	let result = template;
+	if(tag.includes(filler)) return result;
+	while(result.includes(tag))
+		result = result.replace(tag,filler);
+	return result;
+}
+
+const runCommand = async(ssh,command,args,path)=>{
 	if(!args) args = [];
-	return await ssh.exec(command, args, { cwd: '/', stream: 'stdout', options: { pty: true } });
+	if(!path) path = '/';
+	return await ssh.exec(command, args, { cwd: path, stream: 'stdout', options: { pty: true } }).catch(e=>console.error(e));
 }
 
 init();
